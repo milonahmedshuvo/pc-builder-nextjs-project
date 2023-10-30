51dce2341c6c351983261b2b42c947c210a8ef19
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-30 23:57:05 +0600

    pre randaring

--- src/components/ui/randomproducts/Randomproducts.js
+++ src/components/ui/randomproducts/Randomproducts.js
@@ -1,11 +1,14 @@
 
 
-const Randomproducts = () => {
+const Randomproducts = ({prodcuts}) => {
+  console.log(prodcuts)
+
   return (
     <div>
       <h1>random products </h1>
     </div>
   )
 }
+export default Randomproducts;
 
-export default Randomproducts
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,32 @@ import RootLayout from "@/components/layout/RootLayout"
 import Randomproducts from "@/components/ui/randomproducts/Randomproducts";
 
 
-import React from 'react'
 
-const HomePage = () => {
+
+const HomePage = ({prodcuts}) => {
+  
   return (
     <div>
        <h1 className="text-red-400 text-4xl">home page root: </h1>
-       <Randomproducts></Randomproducts>
+       <Randomproducts prodcuts={prodcuts} ></Randomproducts>
     </div>
   )
 }
 
 export default HomePage;
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products")
+  const data = await res.json()
+  
+
+  return {
+    props:{
+      prodcuts: data
+    }
+  }
+}
 
 
 
