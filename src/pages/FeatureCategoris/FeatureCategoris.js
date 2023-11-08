01eda8761cbda6b07b5704d82b27c41069181d09
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-11-08 23:53:47 +0600

    mulltipule fetch

---
+++ src/pages/FeatureCategoris/FeatureCategoris.js
@@ -0,0 +1,20 @@
+
+
+
+const FeatureCategoris = ({categoris}) => {
+   console.log("|ddddddddddd",categoris)
+   
+
+
+  return (
+    <div className='mt-10'>
+        <h4 className='text-center font-semibold font-sans text-xl'>Featured Category</h4>  
+        <p className='text-center mt-1'>Get Your Desired Product from Featured Category!</p>
+    </div>
+  )
+}
+
+export default FeatureCategoris;
+
+
+ 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,39 @@
 import RootLayout from "@/components/layout/RootLayout"
+import FeatureCategoris from "@/pages/FeatureCategoris/FeatureCategoris";
 import Randomproducts from "@/components/ui/randomproducts/Randomproducts";
 
 
 
 
-const HomePage = ({prodcuts}) => {
+const HomePage = ({prodcuts, categoris}) => {
+ console.log("multipule fetch", categoris)
   
   return (
     <div className="">
        <h1 className="text-red-400 text-4xl">home page root: </h1>
        <Randomproducts prodcuts={prodcuts} ></Randomproducts>
+       <FeatureCategoris categoris={categoris} ></FeatureCategoris>
     </div>
   )
 }
 
 export default HomePage;
 
-
 export const getStaticProps = async () => {
-  const res = await fetch("http://localhost:5000/products")
-  const data = await res.json()
+  const res1 = await fetch("http://localhost:5000/products")
+  const data = await res1.json()
   
+  const res2 = await fetch("http://localhost:5000/categoris")
+  const data2 = await res2.json() 
 
   return {
     props:{
-      prodcuts: data
+      prodcuts: data,
+      categoris:data2 
     }
   }
 }
+
 
 
 
