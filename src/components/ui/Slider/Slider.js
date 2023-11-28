c5ade2fc61f335eb0079ab81199fe1ae54956ad3
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-11-29 00:00:11 +0600

    bbb

--- src/components/ui/Slider/Slider.js
+++ src/components/ui/Slider/Slider.js
@@ -27,8 +27,9 @@ const Slider = () => {
 
 
   return (
-    <div>
-      <div className="overflow-hidden w-full">
+    <div className='flex flex-col md:flex-row gap-4 md:gap-10'>
+
+      <div className="overflow-hidden  w-full md:w-3/4">
       <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
         {slides.map((slide, index) => (
           <div key={index} className="min-w-full box-border">
@@ -37,6 +38,27 @@ const Slider = () => {
         ))}
       </div>
     </div>
+
+     {/* second div  */}
+      <div className=' w-full md:w-1/4'>
+
+       {/* item 1  */}
+        <div className='bg-[#FFE8A1] py-5'>
+
+         <div className='mx-10 m-auto'>
+
+         <p className='text-black font-medium text-lg text-center'>Compare Products</p>
+          <p className='text-[#9c9168] text-center'>Choose Two Products to Compare</p>
+
+
+
+          <button type="button" className='text-[#72BF44] border-2 border-[#72BF44] w-full py-2 font-semibold '> View Comparison</button>
+         </div>
+
+
+        </div>
+       
+      </div>
     </div>
   )
 }
