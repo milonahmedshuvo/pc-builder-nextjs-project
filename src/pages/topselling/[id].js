0800a509483394e8920b590ec54e7d259aee0abf
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-09 23:36:22 +0600

    dainamic cmpnt res

--- src/pages/topselling/[id].js
+++ src/pages/topselling/[id].js
@@ -1,31 +1,65 @@
-import React from 'react'
+import RootLayout from "@/components/layout/RootLayout";
+import Link from "next/link";
+import React from "react";
 
-const topsellingDatails = ({products}) => {
-    const { images, title, balance, logo } = products;
-
+const topsellingDatails = ({ products }) => {
+  // console.log(products)
+  const { images, title, balance, logo, discription } = products;
 
   return (
-    <div>
-       <h1>Top selling prodcuts datails </h1>
+    <div className="flex flex-col md:flex-row justify-center mt-10 mb-20">
+      <div className="w-full md:w-full lg:w-1/4 ">
+        <img className="w-full" src={images} alt="" />
+      </div>
+
+      <div className="w-full md:w-full lg:w-3/6 px-10 ">
+        <div className="w-24 ">
+          <img src={logo} alt="" />
+        </div>
+        <p className="text-[#3E3E3E] text-2xl md:text-3xl font-bold lato">{title}</p>
+        <p className=" text-lg md:text-xl text-[#3E3E3E] font-bold mt-4 lato ">{balance}</p>
+        <p className="font-normal text-sm text-[#3E3E3E] lato tracking-wide leading-7 mt-8 ">
+          {discription}.For Datails..
+        </p>
+
+        <p className="text-sm  md:text-md font-semibold mt-4 text-[#3E3E3E]">No return applicable if the seal is broken.</p>
+
+        <div className="grid grid-cols-2 gap-1 md:gap-2 mt-10 ">
+          <Link
+            href="/"
+            className="bg-[#3197E9] text-white border py-3 px-1   font-semibold text-sm md:text-sm lg:text-md   text-center "
+          >
+            <button type="button">ADD TO CART</button>
+          </Link>
+
+          <Link
+            href="/"
+            className="bg-[#F26E21] text-white border py-3 px-1 font-semibold md:text-sm lg:text-md  text-base text-center "
+          >
+            <button type="button">Buy now</button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default topsellingDatails;
 
-
-
 export const getServerSideProps = async (context) => {
-    const { id } = context.params
-    console.log(id)
+  const { id } = context.params;
+  console.log(id);
 
-     const res = await fetch(`http://localhost:5000/topsellingDainamic/${id}`)
-     const data = await res.json()
-     
+  const res = await fetch(`http://localhost:5000/topsellingDainamic/${id}`);
+  const data = await res.json();
 
-     return {
-        props:{
-            products: data
-        }
-     }
+  return {
+    props: {
+      products: data,
+    },
+  };
+};
+
+topsellingDatails.getLayout = function getLayout(page) {
+  return <RootLayout> {page} </RootLayout>;
 };
