324e5e21bf05232a634d4140efe50c846481c67d
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-11 00:07:45 +0600

    speaker home cmlte

--- src/components/ui/Speaker/Speakerhome.js
+++ src/components/ui/Speaker/Speakerhome.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Speakerhome = ({speakerImg}) => {
-    console.log(speakerImg)
+const Speakerhome = ({ speakerImg }) => {
+  console.log(speakerImg);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-1 my-14" >
-      
-      {
-        speakerImg.map((img, i) => <div key={img._id}>
-
-            <img src={img.images} alt="" />
-        </div>)
-      }
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  md:gap-1 mt-14">
+        {speakerImg.map((img, i) => (
+          <div key={img._id} className="flex justify-center">
+            <img className="w-full md:mx-2" src={img.images} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Speakerhome;
