f0b9bc779d8b24a476f885275da0687b61c39bc1
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-10 23:45:56 +0600

    img show

--- src/components/ui/Speaker/Speakerhome.js
+++ src/components/ui/Speaker/Speakerhome.js
@@ -3,8 +3,14 @@ import React from 'react'
 const Speakerhome = ({speakerImg}) => {
     console.log(speakerImg)
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-1 mt-4" >
-      <h1>speaker home page </h1>
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-1 my-14" >
+      
+      {
+        speakerImg.map((img, i) => <div key={img._id}>
+
+            <img src={img.images} alt="" />
+        </div>)
+      }
     </div>
   )
 }
