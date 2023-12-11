b906c4275c19a6810d3e369554981665fa6317d5
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-11 23:33:11 +0600

    six products show

--- src/components/ui/Speaker/Speakerhome.js
+++ src/components/ui/Speaker/Speakerhome.js
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const Speakerhome = ({ speakerImg }) => {
-  console.log(speakerImg);
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  md:gap-1 mt-14">
         {speakerImg.map((img, i) => (
           <div key={img._id} className="flex justify-center">
+            <Link href={`http://localhost:3000/speaker/${img._id}`} > 
             <img className="w-full md:mx-2" src={img.images} alt="" />
+            </Link>
           </div>
         ))}
       </div>
---
+++ src/pages/speaker/[id].js
@@ -0,0 +1,77 @@
+import RootLayout from '@/components/layout/RootLayout';
+import ProductCard from '@/components/ui/Speaker/ProductCard';
+import { data } from 'autoprefixer';
+import React from 'react'
+
+const SpeakerProductDatails = ({speakerProducts}) => {
+    const { products} = speakerProducts;
+    // console.log( " ddddddddd", products)
+    // console.log(speakerProducts)
+
+
+  return (
+    <div>
+        
+        <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
+      <div className="col-span-3 z mr-10 space-y-5 border rounded-2xl border-gray-200/80 p-5 self-start sticky top-16 h-[calc(100vh-80px)]">
+        <div>
+          <h1 className="text-2xl uppercase">Availability</h1>
+          <div className="flex items-center space-x-2 mt-3">
+          <input type="checkbox" className="toggle" checked />
+
+          <input type="checkbox" class="toggle" checked />
+          
+
+            {/* <Switch id="in-stock" /> */}
+            {/* <Label htmlFor="in-stock">In stock</Label> */}
+          </div>
+        </div>
+        <div className="space-y-3 ">
+          <h1 className="text-2xl uppercase">Price Range</h1>
+          <div className="max-w-xl">
+
+          <input type="range" min={0} max="100" value="40" className="range range-primary" />
+
+         
+            {/* <Slider
+              defaultValue={[150]}
+              max={150}
+              min={0}
+              step={1}
+              onValueChange={(value) => handleSlider(value)}
+            /> */}
+          </div>
+          <div>From 0$ To $</div>
+        </div>
+      </div>
+      <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
+        {products?.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default SpeakerProductDatails;
+
+SpeakerProductDatails.getLayout = function getLayout(page) {
+    return <RootLayout> {page} </RootLayout>
+}
+
+
+export const getServerSideProps =async (context) =>{
+    const {id} = context.params 
+
+    const res = await  fetch(`http://localhost:5000/speaker/${id}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            speakerProducts: data
+        }
+    }
+
+}
