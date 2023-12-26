a512b3fc71ccbd55f96c0b67c9ae1b3114145be0
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-26 23:30:09 +0600

    cart responsive

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -32,13 +32,14 @@ const Cart = () => {
        <div className='flex justify-between  w-full mt-14 mb-5'>
                 
                   <div className='w-1/2 '>
-                    Product
+                    <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold ' >Product</p>
+                    
                   </div>
             
                   <div className='w-1/2 flex justify-between items-center'>           
-                       <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold '>QUANTITY</p>
-                        <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold' >OFFER PRICE</p>
-                         <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold' > SUBTOTAL</p>
+                       <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold hidden md:block '>QUANTITY</p>
+                        <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold hidden md:block' >OFFER PRICE</p>
+                         <p className='lato text-sm tracking-wide text-[#3E3E3E] font-bold hidden md:block' > SUBTOTAL</p>
                   </div>
 
             </div>
@@ -51,16 +52,19 @@ const Cart = () => {
 
 
            {
-            products.map((product) =>  <div className='flex justify-between border w-full'>
+            products.map((product) =>  <div className='flex flex-col md:flex-row justify-between border w-full my-6'>
             {/* img */}
-             <div className='flex '>
+             <div className='flex justify-start items-center gap-3 w-full md:w-1/2 border-red-200 border'>
 
-             <div className='border w-1/2 '>
-               <img src={product.images} alt="" />
+             <div className='border w-1/4 '>
+               <img className='' src={product.images} alt="" />
              </div>
 
              <div>
-               <p>{product.title}</p>
+               <p className='text-md my-1 text-[#3E3E3E] lato font-semibold hover:text-[#F79637]'>{product.title}</p>
+               <p className='text-sm my-1 text-[#3E3E3E] lato' >Color : Silver</p>
+               <p className='text-sm my-1 text-[#3E3E3E] lato' >Size : 44mm</p>
+               <button className='text-sm text-[#3E3E3E] lato hover:text-[#F79637]'>Remove</button>
              </div>
 
 
@@ -69,10 +73,10 @@ const Cart = () => {
              
 
              
-             <div className='w-1/2 flex justify-between items-center'>
+             <div className=' w-full md:w-1/2 flex justify-between items-center '>
 
 
-                  <div className='border py-3 px-2 w-40 flex justify-between items-center'>
+                  <div className='border py-3 px-2 w-28 md:w-40 flex justify-between items-center'>
                      <button type="button" className='text-lg' > <FaPlus /> </button>
                       <p className='text-lg' > {product.quantity} </p>
                      <button type="button" className='text-lg' > <FaMinus /> </button>
@@ -81,13 +85,13 @@ const Cart = () => {
 
 
                   <div>
-                      <p className='rubik'> not available </p>
+                      <p className='lato text-sm md:text-md'> not available </p>
                   </div>
 
 
 
                   <div>
-                     <p>Tk: {product.tk}</p>
+                     <p className='text-sm md:text-md lato font-medium mr-4 md:mr-0' >Tk: {product.tk}</p>
                   </div>
 
 
