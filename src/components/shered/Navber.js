589f77b4023c359213681137d675d93db972f90d
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-01-02 23:59:57 +0600

    product done

--- src/components/shered/Navber.js
+++ src/components/shered/Navber.js
@@ -1,46 +1,37 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../image/Tech.png"
-import { useSession, signIn, signOut } from "next-auth/react"
-
-
+import logo from "../../image/Tech.png";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navber = () => {
-     const { data:session } = useSession()
-     console.log("user", session?.user)
-
-     
+  const { data: session } = useSession();
+  console.log("user", session?.user);
 
   return (
     <div className=" bg-[#FFFFFF] shadow-sm flex  justify-between items-center py-4">
       <div>
-        
-        <Link href='/' > 
-        <Image
-          // src="https://www.ryanscomputers.com/assets/images/ryans-logo.svg"
-          src={logo}
-          width={200}
-          height={400}
-          alt="Picture of the author"
-        />
+        <Link href="/">
+          <Image
+            // src="https://www.ryanscomputers.com/assets/images/ryans-logo.svg"
+            src={logo}
+            width={200}
+            height={400}
+            alt="Picture of the author"
+          />
         </Link>
       </div>
 
       <div className="flex justify-between items-center">
         {/* dropdown manu  */}
 
-
-        
-        <Link href='/Allproducts/Allproducts' className="hidden md:block">
-            <button className="rubik hover:text-[#F26E21]">PRODUCTS</button>
+        <Link href="/Allproducts/Allproducts" className="hidden md:block">
+          <button className="rubik hover:text-[#F26E21]">PRODUCTS</button>
         </Link>
 
-
- 
         <div className="right-manu  relative mx-2 md:mx-6 hover:text-[#F26E21]">
           <button type="button" className="manu-btn hover:text-[#F26E21] rubik">
-           CATEGORY
+            CATEGORY
           </button>
           <div className="drowpdown z-10 py-4 border-t-4 border-[#F26E21] shadow-xl absolute hidden min-w-[179px] ">
             <a href="" className="block">
@@ -62,26 +53,27 @@ const Navber = () => {
             <button className="rubik hover:text-[#F26E21]">CART</button>
         </Link> */}
 
-         {
-          session?.user? <Link onClick={()=> signOut() } href='' className="hidden md:block">
-          <button className="rubik hover:text-[#F26E21]">LOGOUT</button>
-      </Link> : <Link href='/login/login' className="hidden md:block">
+        {session?.user ? (
+          <Link onClick={() => signOut()} href="" className="hidden md:block">
+            <button className="rubik hover:text-[#F26E21]">LOGOUT</button>
+          </Link>
+        ) : (
+          <Link href="/login/login" className="hidden md:block">
             <button className="rubik hover:text-[#F26E21]">LOGIN</button>
-        </Link>
-         }
-        
+          </Link>
+        )}
 
         {/* <Link href='/login/login' className="hidden md:block">
             <button className="rubik hover:text-[#F26E21]">LOGIN</button>
         </Link> */}
 
         {/* button  */}
-        <Link href="/pc-builder/PCBuilder" >
-        <div className=" mx-1 md:ml-4 ">
-          <button className="bg-[#F26E21] text-white font-normal md:font-semibold py-1 px-2 md:px-6 rounded">
-            PC Builder
-          </button>
-        </div>
+        <Link href="/pc-builder/PCBuilder">
+          <div className=" mx-1 md:ml-4 ">
+            <button className="bg-[#F26E21] text-white font-normal md:font-semibold py-1 px-2 md:px-6 rounded">
+              PC Builder
+            </button>
+          </div>
         </Link>
       </div>
     </div>
@@ -89,4 +81,3 @@ const Navber = () => {
 };
 
 export default Navber;
-
